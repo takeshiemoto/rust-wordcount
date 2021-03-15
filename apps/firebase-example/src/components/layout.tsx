1ca693dca630f2fa1ce93618e7dcadfa65dca899
Tekeshi Emoto <45647252+takeshiemoto@users.noreply.github.com>
2021-03-15 15:19:28 +0900

    feat: add hook-form singin (#12)

--- apps/firebase-example/src/components/layout.tsx
+++ apps/firebase-example/src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Flex, Link, View } from '@adobe/react-spectrum';
+import { Flex, Header, Link, View } from '@adobe/react-spectrum';
 import React, { ReactNode, VFC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -9,16 +9,24 @@ type Prop = {
 
 export const Layout: VFC<Prop> = ({ children, user }) => {
   return (
-    <Flex minHeight={'100vh'}>
-      {user ? (
-        <View>{children}</View>
-      ) : (
-        <View>
+    <View minHeight={'100vh'} padding={8}>
+      <Header>
+        <Link>
+          <RouterLink to={'/'}>App</RouterLink>
+        </Link>
+        {user ? (
+          <>
+            <Link>
+              <RouterLink to={'/mypage'}>MyPage</RouterLink>
+            </Link>
+          </>
+        ) : (
           <Link>
             <RouterLink to={'/signin'}>Sign In</RouterLink>
           </Link>
-        </View>
-      )}
-    </Flex>
+        )}
+      </Header>
+      <View>{children}</View>
+    </View>
   );
 };
