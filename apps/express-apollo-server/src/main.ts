00ebd31e4d473fda8fddbc56761436d30a0dc81c
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-03-26 09:52:34 +0900

    feat: express with apollo-server setup

--- apps/express-apollo-server/src/main.ts
+++ apps/express-apollo-server/src/main.ts
@@ -1,18 +1,32 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { ApolloServer, gql } from 'apollo-server-express';
 import * as express from 'express';
 
+/**
+ * - クエリを作成する場合は必ず同じ名前のリゾルバ関数を定義する
+ * - リゾルバはスキーマ内のオブジェクトと同じtypename（この例だとtotalPhotos）として定義
+ */
+const typeDefs = gql`
+  type Query {
+    totalPhotos: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42
+  }
+}
+
 const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/graphql'})
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to express-apollo-server!' });
 });
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+}).on('error', console.error);
