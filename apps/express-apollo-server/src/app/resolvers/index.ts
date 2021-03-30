736a38cf315dbe5b2cf5193e65147fa7c3f34420
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-03-30 09:15:25 +0900

    feat: added resolver type

--- apps/express-apollo-server/src/app/resolvers/index.ts
+++ apps/express-apollo-server/src/app/resolvers/index.ts
@@ -1,9 +1,52 @@
-import { Mutation } from './Mutation';
-import { Query } from './Query';
-import { Type } from './Type';
+import { GraphQLScalarType } from 'graphql';
 
-export const resolvers = {
-  Query,
-  Mutation,
-  ...Type,
+import { Photo, Resolvers, User } from '../../types/genereted';
+
+export const resolvers: Resolvers = {
+  Query: {
+    totalPhotos: (parent, args, context) =>
+      context.db.collection('photos').estimatedDocumentCount(),
+    allPhotos: (parent, args, context) =>
+      context.db.collection('photos').find().toArray(),
+    totalUsers: (parent, args, context) =>
+      context.db.collection('users').estimatedDocumentCount(),
+    allUsers: (parent, args, context) =>
+      context.db.collection('users').find().toArray(),
+  },
+  Mutation: {
+    postPhoto: () => {
+      return {} as Photo;
+    },
+  },
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: () => {
+      // return USERS.find((u) => u.githubLogin === parent.id);
+      return {} as User;
+    },
+    // taggedUser: (parent) =>
+    //   TAGS.filter((tag) => tag.photoID === parent.id)
+    //     .map((tag) => tag.userID)
+    //     .map((userId) => USERS.find((u) => u.githubLogin === userId)),
+    taggedUser: () => [],
+  },
+  User: {
+    // postedPhotos: (parent) => {
+    //   return PHOTOS.filter((p) => p.githubUser === parent.githubLogin);
+    // },
+    postedPhotos: () => [],
+    inPhotos: () => [],
+    // inPhotos: (parent) => {
+    //   return TAGS.filter((tag) => tag.userID === parent.id)
+    //     .map((tag) => tag.photoID)
+    //     .map((photoId) => PHOTOS.find((p) => p.id === photoId));
+    // },
+  },
+  DateTime: new GraphQLScalarType({
+    name: `DateTime`,
+    description: `A valid date time value.`,
+    parseValue: (value) => new Date(value),
+    serialize: (value) => new Date(value).toISOString(),
+    parseLiteral: (ast: any) => ast.value,
+  }),
 };
