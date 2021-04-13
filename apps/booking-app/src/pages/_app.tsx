b50371e6e10d3a25068ae60953aee042437f44ea
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-04-13 15:47:29 +0900

    feat: booking-app init

--- apps/booking-app/src/pages/_app.tsx
+++ apps/booking-app/src/pages/_app.tsx
@@ -1,14 +1,23 @@
+import { ThemeProvider } from '@material-ui/core';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+
+import { theme } from '../theme';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to booking-app!</title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
--- apps/booking-app/src/pages/index.tsx
+++ apps/booking-app/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 
 export function Index() {
-  return <div>Booking App</div>;
+  return (
+    <div>
+      <Button color={'secondary'} variant={'outlined'}>
+        Hello world
+      </Button>
+    </div>
+  );
 }
 
 export default Index;
