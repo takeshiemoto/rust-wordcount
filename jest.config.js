cbe122db171b493243ac0ffca51ca76a2ce82852
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-04-23 05:47:54 +0900

    chore: remove apps

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/apps/redux-toolkit-example',
     '<rootDir>/apps/firebase-example',
-    '<rootDir>/apps/next-with-nrql',
     '<rootDir>/apps/nest-graphql-api',
     '<rootDir>/apps/next-apollo',
   ],
