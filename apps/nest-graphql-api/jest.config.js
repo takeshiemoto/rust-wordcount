5ac57aaf248999920b7786d3d0ee8e036ec4257a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-05-16 16:14:10 +0900

    feat: dev

--- apps/nest-graphql-api/jest.config.js
+++ apps/nest-graphql-api/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   displayName: 'nest-graphql-api',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-    },
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
